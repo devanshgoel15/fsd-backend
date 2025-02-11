f026727384132a6ff7d2d1c260575bf19a20fae2
devanshgoel15 <goel.devansh169@gmail.com>
2025-02-11 11:58:14 +0530

    read write unlink make and dellete directory

---
+++ day2/p6.js
@@ -0,0 +1,9 @@
+  const fs =require("fs");
+  fs.rmdir("mydir",(e)=>{
+    if (e){
+        console.error("error deleting directory:" , e);
+        return ;
+    }
+    console.log("directory deleted");
+
+  })
